f03cb51f9dc0fc355316a81ee0bdd24fa96098fa
notLucid <103009610+notLucidi@users.noreply.github.com>
2024-11-05 16:58:22 +0700

    Update World_Finder.lua

--- RGT/World_Finder.lua
+++ RGT/World_Finder.lua
@@ -8,140 +8,134 @@ length = 5
 prefix = ""
 suffix = ""
 console_prefix = "`b[`9L`wuciu`9S`b]`9 "
+targetIDs = {3918, 202, 242, 206, 204}
+requiredItems = {}
 
 Lucid =
     [[
     {
-  "sub_name": "World Finder",
-  "icon": "Public",
-  "menu": [
-    {
-      "type": "labelapp",
-      "icon": "Settings",
-      "text": "World Finder - V1"
-    },
-    {
-      "type": "divider"
-    },
-    {
-      "type": "dialog",
-      "text": "Customize",
-      "support_text": "Settings World Finder Here",
-      "fill": true,
+      "sub_name": "World Finder",
+      "icon": "Public",
       "menu": [
         {
           "type": "labelapp",
-          "text": "Customize World Finder",
-          "icon": "Public"
-        },
-        {
-          "type": "label",
-          "text": "Script by Lucius x SveGTPS"
+          "icon": "Settings",
+          "text": "World Finder - V1"
         },
         {
           "type": "divider"
         },
         {
-          "type": "input_string",
-          "text": "Set Prefix",
-          "default": "]] ..
-    prefix ..
-        [[",
-          "placeholder": "Example: STORAGE",
-          "icon": "Edit",
-          "alias": "config_prefix"
-        },
-        {
-          "type": "tooltip",
-          "icon": "tips_icon",
-          "text": "Hint :",
-          "support_text": "More Example: prefix + world name, output : STORAGEVED"
+          "type": "dialog",
+          "text": "Customize",
+          "support_text": "Settings World Finder Here",
+          "fill": true,
+          "menu": [
+            {
+              "type": "labelapp",
+              "text": "Customize World Finder",
+              "icon": "Public"
+            },
+            {
+              "type": "label",
+              "text": "Script by Lucius x SveGTPS"
+            },
+            {
+              "type": "divider"
+            },
+            {
+              "type": "input_string",
+              "text": "Set Prefix",
+              "default": "]] .. prefix .. [[",
+              "placeholder": "Example: STORAGE",
+              "icon": "Edit",
+              "alias": "config_prefix"
+            },
+            {
+              "type": "tooltip",
+              "icon": "tips_icon",
+              "text": "Hint :",
+              "support_text": "More Example: prefix + world name, output : STORAGEVED"
+            },
+            {
+              "type": "divider"
+            },
+            {
+              "type": "input_string",
+              "text": "Set Suffix",
+              "default": "]] .. suffix .. [[",
+              "placeholder": "Example: FARM",
+              "icon": "Edit",
+              "alias": "config_suffix"
+            },
+            {
+              "type": "tooltip",
+              "icon": "tips_icon",
+              "text": "Hint :",
+              "support_text": "More Example: world name + suffix, output : DKOFARM"
+            },
+            {
+              "type": "divider"
+            },
+            {
+              "type": "input_string",
+              "text": "Set Prefix",
+              "default": "]] .. mode .. [[",
+              "placeholder": "Example: NN",
+              "icon": "Edit",
+              "alias": "config_mode"
+            },
+            {
+              "type": "tooltip",
+              "icon": "tips_icon",
+              "text": "Hint :",
+              "support_text": "No Number (NN) | With Number (WN) | Number Only (NO)"
+            },
+            {
+              "type": "divider"
+            },
+            {
+              "text": "Length",
+              "type": "slider",
+              "default": "]] .. length .. [[",
+              "min": 1,
+              "max": 24,
+              "alias": "config_length"
+            },
+            {
+              "type": "tooltip",
+              "icon": "tips_icon",
+              "text": "Hint :",
+              "support_text": "Set Length to generate 1 or more up to 24 letters."
+            },
+            {
+              "type": "divider"
+            },
+            {
+              "type": "labelapp",
+              "text": "Lucius",
+              "icon": "Handyman"
+            },
+            {
+              "type": "divider"
+            }
+          ]
         },
         {
           "type": "divider"
         },
         {
-          "type": "input_string",
-          "text": "Set Suffix",
-          "default": "]] ..
-            suffix ..
-                [[",
-          "placeholder": "Example: FARM",
-          "icon": "Edit",
-          "alias": "config_suffix"
+          "type": "button",
+          "text": "Generate",
+          "alias": "button_generate"
         },
         {
-          "type": "tooltip",
-          "icon": "tips_icon",
-          "text": "Hint :",
-          "support_text": "More Example: world name + suffix, output : DKOFARM"
-        },
-        {
-          "type": "divider"
-        },
-        {
-          "type": "input_string",
-          "text": "Set Prefix",
-          "default": "]] ..
-                    mode ..
-                        [[",
-          "placeholder": "Example: NN",
-          "icon": "Edit",
-          "alias": "config_mode"
-        },
-        {
-          "type": "tooltip",
-          "icon": "tips_icon",
-          "text": "Hint :",
-          "support_text": "No Number (NN) | With Number (WN) | Number Only (NO)"
-        },
-        {
-          "type": "divider"
-        },
-        {
-          "text": "Length",
-          "type": "slider",
-          "default": "]] ..
-                            length ..
-                                [[",
-          "min": 1,
-          "max": 24,
-          "alias": "config_length"
-        },
-        {
-          "type": "tooltip",
-          "icon": "tips_icon",
-          "text": "Hint :",
-          "support_text": "Set Length to generate 1 or more up to 24 letters."
-        },
-        {
-          "type": "divider"
-        },
-        {
-          "type": "labelapp",
-          "text": "Lucius",
-          "icon": "Handyman"
-        },
-        {
-          "type": "divider"
+          "type": "button",
+          "text": "Scan",
+          "alias": "button_scan"
         }
       ]
-    },
-    {
-      "type": "divider"
-    },
-    {
-      "type": "button",
-      "text": "Generate",
-      "alias": "button_generate"
-    },
-    {
-      "type": "button",
-      "text": "Scan",
-      "alias": "button_scan"
     }
-  ]
-}
 ]]
 addIntoModule(Lucid)
 
@@ -154,7 +148,7 @@ function overlay(message)
 end
 
 function saveLogs(data)
-    file = io.open("/storage/emulated/0/Android/data/launcher.powerkuy.growlauncher/ScriptLua/LogGenerate.db", "a")
+    file = io.open("/storage/emulated/0/Android/data/launcher.powerkuy.growlauncher/ScriptLua/LogGenerate.db", "w") 
     if file then
         file:write(data)
         file:close()
@@ -186,31 +180,31 @@ function generateWorld()
     sendPacket(3, "action|join_request\nname|" .. world .. "\ninvitedWorld|0")
 end
 
-function formater(id, count)
+function generateItems(id, count)
     local itemName = growtopia.getItemName(id) or "Unknown"
-    return string.format("add_button_with_icon|info_%d|%d|frame|%d|%d|\n", id, count, id, count)
+    return string.format("add_button_with_icon|info_%d|`4%s : %d |frame|%d|%d|\n", id, itemName, count, id, count)
 end
 
-function scannedResult()
+function displayRequiredItemsDialog()
     local items = ""
     local totalItems = 0
-
-    for itemid, count in pairs(setDefault) do
+    
+    for itemid, count in pairs(requiredItems) do
         if count > 0 then
-            items = items .. formater(itemid, count)
+            items = items .. generateItems(itemid, count)
             totalItems = totalItems + 1
         end
     end
 
-    local baseDialog =
-        [[
-add_label_with_icon|big|`bScan Result``|left|7188|
+    local baseDialog = [[
+add_label_with_icon|big|`bScanned Result``|left|7188|
 add_smalltext|Script by `6@SveGTPS`4 x `6@Lucius``|
 add_spacer|small|
-add_smalltext|`oResults Found: `w]] ..totalItems ..[[ `oitems|left|
+add_smalltext|`oResults Found: `w]] .. totalItems .. [[ `oitems|left|
 add_spacer|small|
 end_dialog|req|Cancel|| 
-add_spacer|big|]] .. items .. [[
+add_spacer|big|
+]] .. items .. [[
 add_quick_exit|
 ]]
     sendVariant({v1 = "OnDialogRequest", v2 = baseDialog})
@@ -221,12 +215,12 @@ function scanTiles()
 
     for _, tile in ipairs(tiles) do
         local id = tile.fg
-        if setDefault[id] ~= nil then
-            setDefault[id] = setDefault[id] + 1
+        if requiredItems[id] ~= nil then
+            requiredItems[id] = requiredItems[id] + 1
         end
     end
 
-    scannedResult()
+    displayRequiredItemsDialog()
 end
 
 function onValue(t, n, v)
@@ -250,21 +244,14 @@ function onValue(t, n, v)
     end
     if t == 0 then
         if n == "button_generate" then
-            local time = os.time()
-            local cooldown = 5
-            local executed = 0
-
-            if time - lastExecuted >= cooldown then
-                generateWorld()
-                lastExecuted = time
-            else
-                local remainingTime = cooldown - (currentTime - lastExecuted)
-                console("Cooldown: " .. remainingTime .. " sec left.")
-            end
+            generateWorld()
         end
 
         if n == "button_scan" then
-            scanTiles()
+for _, id in ipairs(targetIDs) do
+    requiredItems[id] = 0
+end
+            scanTiles() 
         end
     end
 end
